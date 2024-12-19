5ed1795c6ddd5242e195a45bb8f9d0d64ae64970
CodewithNishchal <nischal100per@gmail.com>
2024-12-20 02:41:23 +0530

    Major development on start logic

--- Players/Player_4/main.js
+++ Players/Player_4/main.js
@@ -2,6 +2,46 @@
 var random;
 let dice_beads = document.querySelector('.dice-numbers>div').classList;
 var play = document.querySelector("button#play");
+var main_box = document.querySelector("#main-box");
+var interrupt = document.querySelector(".interrupt");
+var player = document.querySelectorAll(".color-pallete > .color-box");
+var player1 = null;
+var player2 = null;
+
+setTimeout(function () {
+    interrupt.style.visibility = "visible";
+}, 1000);
+
+for (let i = 0; i < player.length; i++) { 
+    player[i].addEventListener('click', function () {
+        
+        player[i].classList.add('pressed');
+        setTimeout(function () {
+            player[i].classList.remove('pressed');
+        }, 100);
+        if (player1 === null) {
+            player1 = this.id;
+            document.querySelector('div#interrupt-box > h2').innerHTML = "Choose Player 2";
+        }
+        else if (this.id === player1) {
+            alert("Player 2 cannot be the same as Player 1");
+        }
+        else {
+            player2 = this.id;
+            setTimeout(function () {
+                main_box.style.display = "grid";
+                interrupt.style.display = "none";
+                setTimeout(function () {
+                    play.style.visibility = "visible";
+                }, 1000);
+            }, 1000);
+        }
+    })
+}
+
+play.addEventListener("click", function () {
+    rotateDice(); 
+});
 
 function AddRemove(i,callback)
 {
